2ba3519dc8ea3cbac7eebec54fc8c7c6b28b8ae2
arthur360cgf <arthurmattei115@gmail.com>
2022-10-06 09:32:58 -0300

    adicionando tela de cadastro do cliente no sistema de criaçao para o mercado

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -12,18 +12,18 @@ import br.com.cod3r.exerciciossb.models.Cliente;
 @RequestMapping("/clientes")
 public class ClienteController {
 	
-	@GetMapping("/qualquer")
-	public Cliente obterCliente() {
+	@GetMapping("/anyone")
+	public Cliente pickCustomer() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
 	}
 	
 	@GetMapping("/{id}")
-	public Cliente obterClientePorId1(@PathVariable int id) {
+	public Cliente tackCustomerbyId1(@PathVariable int id) {
 		return new Cliente(id, "Maria", "987.654.321-00");
 	}
 	
 	@GetMapping
-	public Cliente obterClientePorId2(
+	public Cliente tackCustomerbyId2(
 			@RequestParam(name = "id", defaultValue = "1") int id) {
 		return new Cliente(id,"João Augusto", "111.222.333-44");
 	}
